e34fa940b348470d182bf4912540654c1f014725
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-04-18 19:48:42 +0530

    values file added

---
+++ app/src/main/java/com/example/scheduler/values/CustomValues.kt
@@ -0,0 +1,13 @@
+package com.example.scheduler.values
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object PaddingCustomValues {
+    val externalSpacing = 10.dp
+    val internalSpacing = 4.dp
+}
+
+object FontSizeCustomValues {
+    val medium = 16.sp
+}
