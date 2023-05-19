f399e5e6e3efcf2d6ff8bf09ee57aee9d59b499e
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-05-19 11:11:19 +0530

    changed location of add tasks page and main screen, made optimizations to the main screen composables

--- app/src/main/java/com/example/scheduler/ui/screens/AddTaskPage.kt
+++ app/src/main/java/com/example/scheduler/ui/screens/AddTaskPage.kt
@@ -1,4 +1,4 @@
-package com.example.scheduler
+package com.example.scheduler.ui.screens
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
---
+++ app/src/main/java/com/example/scheduler/ui/screens/MainScreen.kt
@@ -0,0 +1,115 @@
+package com.example.scheduler.ui.screens
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.scheduler.values.PaddingCustomValues
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(heightDp = 800)
+fun MainScreen() {
+    val lazyListState = rememberLazyListState()
+    val showFullText = remember { mutableStateOf(true) }
+    LaunchedEffect(lazyListState) {
+        snapshotFlow { lazyListState.firstVisibleItemIndex }
+            .collect { showFullText.value = (it == 0) }
+    }
+    Scaffold(
+        floatingActionButton = { AddTaskFAB(showFullText = showFullText.value) },
+        content = {
+            ListOfTasks(
+                padding = it,
+                lazyListState = lazyListState
+            )
+        }
+    )
+}
+
+@Composable
+fun AddTaskFAB(showFullText: Boolean) {
+    FloatingActionButton(
+        onClick = {
+            // TODO: move to add tasks page
+        },
+        shape = CircleShape,
+        content = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(PaddingCustomValues.externalSpacing)
+            ) {
+                AnimatedVisibility(
+                    visible = showFullText,
+                    modifier = Modifier.padding(start = PaddingCustomValues.externalSpacing),
+                ) {
+                    Text(text = "Add Task")
+                }
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = null,
+                    tint = Color.Blue
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun ListOfTasks(padding: PaddingValues, lazyListState: LazyListState) {
+    // TODO: use lazy list to display tasks after sorting and remove this sample column
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(padding),
+        state = lazyListState,
+        contentPadding = PaddingValues(vertical = PaddingCustomValues.internalSpacing)
+    ) {
+        // TODO: show all tasks
+        items(
+            count = 20,
+            itemContent = {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(PaddingCustomValues.internalSpacing)
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = PaddingCustomValues.externalSpacing,
+                                vertical = 15.dp
+                            ),
+                        text = "sample text $it",
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+        )
+    }
+}
