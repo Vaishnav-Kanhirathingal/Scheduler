9ec65255d692b1b4b547a9db96e0062542e8c854
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-05-20 12:22:25 +0530

    firebase functions from new file replaces older code, some small changes

--- app/src/main/java/com/example/scheduler/MainActivity.kt
+++ app/src/main/java/com/example/scheduler/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.scheduler
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -18,6 +16,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.scheduler.firebase.AccountFunctions
 import com.example.scheduler.ui.destinations.AddTaskScreen
 import com.example.scheduler.ui.destinations.DetailsScreen
 import com.example.scheduler.ui.destinations.MainScreen
@@ -27,7 +26,6 @@ import com.example.scheduler.ui.theme.SchedulerTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : ComponentActivity() {
     private val TAG = this::class.java.simpleName
@@ -45,26 +43,16 @@ class MainActivity : ComponentActivity() {
                     val navHostController: NavHostController = rememberNavController()
                     SchedulerNavHost(
                         navController = navHostController,
-                        modifier = Modifier
+                        modifier = Modifier,
+                        googleSignInButton = { GoogleSignInButton() }
                     )
-                    googleSignInButton()
                 }
             }
         }
     }
 
-    private fun firebaseAuthenticate(idToken: String?) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential).addOnSuccessListener {
-            val user = auth.currentUser
-            Log.d(TAG, "user = ${user?.email}")
-        }.addOnFailureListener {
-            it.printStackTrace()
-        }
-    }
-
     @Composable
-    fun googleSignInButton() {
+    fun GoogleSignInButton() {
         val googleSignInOptions = GoogleSignInOptions
             .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
@@ -75,20 +63,7 @@ class MainActivity : ComponentActivity() {
         val startForResult =
             rememberLauncherForActivityResult(
                 contract = ActivityResultContracts.StartActivityForResult(),
-                onResult = { result: ActivityResult ->
-                    GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                        .addOnSuccessListener {
-                            val credential = GoogleAuthProvider.getCredential(it.idToken, null)
-                            auth.signInWithCredential(credential).addOnSuccessListener {
-                                val user = auth.currentUser
-                                Log.d(TAG, "user = ${user?.email}")
-                            }.addOnFailureListener { e ->
-                                e.printStackTrace()
-                            }
-                        }.addOnFailureListener {
-                            it.printStackTrace()
-                        }
-                }
+                onResult = AccountFunctions.signInLambda
             )
         // TODO: create a login button with the onclick
         Button(
@@ -99,18 +74,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SchedulerNavHost(navController: NavHostController, modifier: Modifier) {
+fun SchedulerNavHost(
+    navController: NavHostController,
+    modifier: Modifier,
+    googleSignInButton: @Composable () -> Unit
+) {
     NavHost(
         navController = navController,
         startDestination = MainScreen.route,
         modifier = modifier,
         builder = {
             composable(route = MainScreen.route) {
-                MainScreen(toAddTaskScreen = { navController.navigate(AddTaskScreen.route) })
+                MainScreen(
+                    toAddTaskScreen = { navController.navigate(AddTaskScreen.route) },
+                    googleSignInButton = googleSignInButton
+                )
             }
             composable(route = AddTaskScreen.route) {
                 // TODO: navigate up after adding the new task
-                AddTaskScreen()
+                AddTaskScreen{
+                    // TODO: navigate up after completion and save the task
+                }
             }
             composable(route = DetailsScreen.route) {
                 // TODO: add details screen
