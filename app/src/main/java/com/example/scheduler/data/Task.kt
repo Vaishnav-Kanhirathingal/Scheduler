2817a682ed9e4edfa970658c95d9afc188f05031
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-05-21 21:47:50 +0530

    added AI generated list of tasks for testing

--- app/src/main/java/com/example/scheduler/data/Task.kt
+++ app/src/main/java/com/example/scheduler/data/Task.kt
@@ -50,3 +50,147 @@ class Time(
     val hour: Int,
     val minute: Int
 )
+
+/** delete this object, do not use in finished project */
+val testTaskList = mutableListOf(
+    Task(
+        title = "Meeting",
+        description = "Discuss project updates",
+        timeForReminder = Time(hour = 9, minute = 30),
+        dateForReminder = Date(dayOfMonth = 15, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 7,
+        snoozeDuration = 10,
+        postponeDuration = 15
+    ),
+    Task(
+        title = "Birthday",
+        description = "Buy a gift",
+        timeForReminder = Time(hour = 18, minute = 0),
+        dateForReminder = Date(dayOfMonth = 10, month = 9, year = 2023),
+        dateWise = true,
+        repeatGapDuration = 0,
+        snoozeDuration = 5,
+        postponeDuration = 10
+    ),
+    Task(
+        title = "Dentist Appointment",
+        description = "Get a dental checkup",
+        timeForReminder = Time(hour = 14, minute = 30),
+        dateForReminder = Date(dayOfMonth = 25, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 15,
+        postponeDuration = 30
+    ),
+    Task(
+        title = "Meeting",
+        description = "Discuss project updates",
+        timeForReminder = Time(hour = 9, minute = 30),
+        dateForReminder = Date(dayOfMonth = 15, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 7,
+        snoozeDuration = 10,
+        postponeDuration = 15
+    ),
+    Task(
+        title = "Birthday",
+        description = "Buy a gift",
+        timeForReminder = Time(hour = 18, minute = 0),
+        dateForReminder = Date(dayOfMonth = 10, month = 9, year = 2023),
+        dateWise = true,
+        repeatGapDuration = 0,
+        snoozeDuration = 5,
+        postponeDuration = 10
+    ),
+    Task(
+        title = "Dentist Appointment",
+        description = "Get a dental checkup",
+        timeForReminder = Time(hour = 14, minute = 30),
+        dateForReminder = Date(dayOfMonth = 25, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 15,
+        postponeDuration = 30
+    ),
+    Task(
+        title = "Gym Workout",
+        description = "Cardio and strength training",
+        timeForReminder = Time(hour = 8, minute = 0),
+        dateForReminder = Date(dayOfMonth = 22, month = 5, year = 2023),
+        dateWise = true,
+        repeatGapDuration = 7,
+        snoozeDuration = 5,
+        postponeDuration = 10
+    ),
+    Task(
+        title = "Project Deadline",
+        description = "Submit final report",
+        timeForReminder = Time(hour = 12, minute = 0),
+        dateForReminder = Date(dayOfMonth = 30, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 0,
+        postponeDuration = 0
+    ),
+    Task(
+        title = "Shopping",
+        description = "Buy groceries",
+        timeForReminder = Time(hour = 10, minute = 0),
+        dateForReminder = Date(dayOfMonth = 23, month = 5, year = 2023),
+        dateWise = true,
+        repeatGapDuration = 7,
+        snoozeDuration = 10,
+        postponeDuration = 20
+    ),
+    Task(
+        title = "Meeting",
+        description = "Discuss project updates",
+        timeForReminder = Time(hour = 9, minute = 30),
+        dateForReminder = Date(dayOfMonth = 15, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 7,
+        snoozeDuration = 10,
+        postponeDuration = 15
+    ),
+    Task(
+        title = "Movie Night",
+        description = "Watch a new release",
+        timeForReminder = Time(hour = 20, minute = 30),
+        dateForReminder = Date(dayOfMonth = 27, month = 5, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 0,
+        postponeDuration = 0
+    ),
+    Task(
+        title = "Anniversary",
+        description = "Plan a surprise",
+        timeForReminder = Time(hour = 18, minute = 0),
+        dateForReminder = Date(dayOfMonth = 10, month = 6, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 15,
+        postponeDuration = 30
+    ),
+    Task(
+        title = "Doctor's Appointment",
+        description = "Follow-up on test results",
+        timeForReminder = Time(hour = 11, minute = 30),
+        dateForReminder = Date(dayOfMonth = 5, month = 6, year = 2023),
+        dateWise = false,
+        repeatGapDuration = 0,
+        snoozeDuration = 0,
+        postponeDuration = 0
+    ),
+    Task(
+        title = "Conference Call",
+        description = "Discuss upcoming projects",
+        timeForReminder = Time(hour = 16, minute = 0),
+        dateForReminder = Date(dayOfMonth = 8, month = 6, year = 2023),
+        dateWise = true,
+        repeatGapDuration = 14,
+        snoozeDuration = 10,
+        postponeDuration = 20
+    )
+)
