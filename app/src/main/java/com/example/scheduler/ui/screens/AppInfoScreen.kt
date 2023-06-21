db533ba7608ff4edc730f21912d875a5fe21687a
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-06-21 14:20:00 +0530

    reformat

--- app/src/main/java/com/example/scheduler/ui/screens/AppInfoScreen.kt
+++ app/src/main/java/com/example/scheduler/ui/screens/AppInfoScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -49,6 +48,7 @@ fun AppInfoScreenPrev() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppInfoScreen(back: () -> Unit) {
+    // TODO: add correct description
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -98,7 +98,7 @@ fun AppInfoContent(paddingValues: PaddingValues) {
                                 )
                             )
 
-                                  },
+                        },
                         content = {
                             Icon(
                                 modifier = Modifier.padding(end = PaddingCustomValues.mediumSpacing),
