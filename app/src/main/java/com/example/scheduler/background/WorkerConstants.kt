10dd69699856cad45117c388d3034581b102b717
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2023-06-23 11:57:50 +0530

    created basic WorkerConstants object

---
+++ app/src/main/java/com/example/scheduler/background/WorkerConstants.kt
@@ -0,0 +1,12 @@
+package com.example.scheduler.background
+
+object WorkerConstants {
+    object CollectiveWorker {
+        const val taskKey = "task_key"
+        const val documentIDKey = "document_id"
+    }
+
+    object TaskWorker {
+        const val notificationIdKey = "notification_id_key"
+    }
+}
